c1d446c872322d212ccc6192392772099c51801a
Victor <victor.herrera.sc@itszapopan.edu.mx>
2019-09-30 22:04:50 -0500

    Feature, now items are added to a firebase backend

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -6,21 +6,25 @@ import IngredientList from "./IngredientList";
 
 function Ingredients() {
   const [ingredientList, setIngredientList] = useState([]);
-  const addIngredientHandler = ingredient => {
-    setIngredientList(oldIngredientList => [
-      ...oldIngredientList,
-      { id: Math.random().toString(), ...ingredient }
-    ]);
+  const addIngredientHandler = async ingredient => {
+    try{
+      const response = await fetch('https://react-burger-builder-8a5cb.firebaseio.com/items.json',{
+        method: 'POST',
+        body: JSON.stringify(ingredient),
+        headers: { 'Content-Type': 'application/json' }
+      });
+      const responseData = await response.json();
+  
+      setIngredientList(oldIngredientList => [
+        ...oldIngredientList,
+        { id: responseData.name, ...ingredient }
+      ]);
+    }catch(error){
+      alert(String.toString(error));
+    }
   };
   const removeIngredientHandler = id => {
-    setIngredientList(oldIngredientList => {
-      const indexOfIngredient = oldIngredientList.findIndex(
-        currentValue => currentValue.id === id
-      );
-      const updatedIngredientList = oldIngredientList;
-      updatedIngredientList.splice(indexOfIngredient, 1);
-      return updatedIngredientList;
-    });
+    setIngredientList(oldIngredientList => oldIngredientList.filter((ingredient)=> ingredient.id !== id));
   };
 
   return (
